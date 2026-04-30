2b2d19ef02c440d3b61aa92000759ac39ffa2947
davidemessi <davide.messi@itiscuneo.eu>
2026-04-30 17:01:09 +0200

    Update partite-config.js

--- partite-config.js
+++ partite-config.js
@@ -2,7 +2,7 @@
 // Lanciare 'python genera_config.py' per aggiornare
 
 const FOTO_CONFIG = {
-  "2": [
+  "1": [
     {
       "publicId": "promo_photos_logo_ck2pvq",
       "thumb": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_300,h_200,c_fill,q_auto,f_auto/promo_photos_logo_ck2pvq",
@@ -17,6 +17,23 @@ const FOTO_CONFIG = {
       "publicId": "ESEMPI_DI_FRAME_DA_APPENDERE_A_SPORTAREA_2_bf9lcm",
       "thumb": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_300,h_200,c_fill,q_auto,f_auto/ESEMPI_DI_FRAME_DA_APPENDERE_A_SPORTAREA_2_bf9lcm",
       "full": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_1400,q_auto,f_auto/ESEMPI_DI_FRAME_DA_APPENDERE_A_SPORTAREA_2_bf9lcm"
+    }
+  ],
+  "2": [
+    {
+      "publicId": "bebe_sfondo_bianco_yta4r7",
+      "thumb": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_300,h_200,c_fill,q_auto,f_auto/bebe_sfondo_bianco_yta4r7",
+      "full": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_1400,q_auto,f_auto/bebe_sfondo_bianco_yta4r7"
+    },
+    {
+      "publicId": "logo_promosport_no_sfondo_tivwsb",
+      "thumb": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_300,h_200,c_fill,q_auto,f_auto/logo_promosport_no_sfondo_tivwsb",
+      "full": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_1400,q_auto,f_auto/logo_promosport_no_sfondo_tivwsb"
+    },
+    {
+      "publicId": "logo_minibasket_senza_sfondo_msmbka",
+      "thumb": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_300,h_200,c_fill,q_auto,f_auto/logo_minibasket_senza_sfondo_msmbka",
+      "full": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_1400,q_auto,f_auto/logo_minibasket_senza_sfondo_msmbka"
     }
   ]
 };
