b321740d453189771aac451349470cc9eb89a409
davidemessi <davide.messi@itiscuneo.eu>
2026-04-30 22:23:41 +0200

    file

--- partite-config.js
+++ partite-config.js
@@ -2,11 +2,5 @@
 // Lanciare 'python genera_config.py' per aggiornare
 
 const FOTO_CONFIG = {
-  "1": [
-    {
-      "publicId": "ESEMPI_DI_FRAME_DA_APPENDERE_A_SPORTAREA_3_igwxph",
-      "thumb": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_300,h_200,c_fill,q_auto,f_auto/ESEMPI_DI_FRAME_DA_APPENDERE_A_SPORTAREA_3_igwxph",
-      "full": "https://res.cloudinary.com/dsl5rkm7q/image/upload/w_1400,q_auto,f_auto/ESEMPI_DI_FRAME_DA_APPENDERE_A_SPORTAREA_3_igwxph"
-    }
-  ]
+
 };
